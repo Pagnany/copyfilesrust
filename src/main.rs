9eda9cc3a9c6fd65b9d4dc4201c94a68a6074d33
Pagnany <eric.schmale@gmx.de>
2023-11-14 09:48:00 +0100

    .

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use csv;
+use rayon::prelude::*;
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let path = args[1].clone();
+    let mut rdr = csv::ReaderBuilder::new()
+        .delimiter(b';')
+        .from_path(path)
+        .expect("Cannot read file");
+
+    let records: Vec<_> = rdr
+        .records()
+        .collect::<Result<_, _>>()
+        .expect("Cannot read records");
+
+    records.par_iter().enumerate().for_each(|(i, result)| {
+        if let Some(source_path) = result.get(0) {
+            if let Some(destination_path) = result.get(1) {
+                match fs::copy(source_path, destination_path) {
+                    Ok(_) => { //println!("{}: {} -> {}", i, source_path, destination_path)
+                    }
+                    Err(e) => {
+                        println!("{}: {} -> {} ({})", i, source_path, destination_path, e)
+                    }
+                }
+            }
+        }
+    });
+}
