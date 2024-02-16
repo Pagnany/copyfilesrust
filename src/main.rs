c63f0036b39700ec98f4ca60faece1af45b7ce30
Pagnany <eric.schmale@gmx.de>
2024-02-16 07:49:25 +0100

    check if file already exists. if so do not copy.

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,15 @@ fn main() {
     records.par_iter().enumerate().for_each(|(i, result)| {
         if let Some(source_path) = result.get(0) {
             if let Some(destination_path) = result.get(1) {
+                // check if the file already exists
+                if fs::metadata(destination_path).is_ok() {
+                    println!(
+                        "{}: {} -> {} (already exists)",
+                        i, source_path, destination_path
+                    );
+                    return;
+                }
+                // copy the file
                 match fs::copy(source_path, destination_path) {
                     Ok(_) => { //println!("{}: {} -> {}", i, source_path, destination_path)
                     }
